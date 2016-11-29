3cc6a9dede0fae977100c6f8a36f28b860344e7a
Scott McCain <jspmccain@gmail.com>
2016-11-29 11:16:41 -0400

    Add files via upload

---
+++ search_R_dictionary.R
@@ -0,0 +1,7 @@
+#I am an experienced R user, but am starting to get into Python. This is a running list of the frustrations and/or translations, where I think "I know exactly how to do this in R, what is the equivalent function?!! #$%@"
+
+search_R <- function(R_function, dictionary){
+  #search the dictionary for the R function in mind, return the entry.
+  #Add a dynamic and/or typo correction.
+  #Put csv file of the dictionary online. 
+}
